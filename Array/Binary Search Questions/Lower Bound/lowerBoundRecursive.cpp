8df39541e267784156add0b434d5d4ba3cc722d6
omeirr13 <74139343+omeirr13@users.noreply.github.com>
2023-09-22 14:09:59 +0500

    Update lowerBoundRecursive.cpp

--- Array/Binary Search Questions/Lower Bound/lowerBoundRecursive.cpp
+++ Array/Binary Search Questions/Lower Bound/lowerBoundRecursive.cpp
@@ -1,1 +1,14 @@
 
+int helper(vector<int>& arr, int low, int high, int x)
+{
+	if(low>high) return low;
+	int mid = low + (high - low) / 2;
+	if(x <= arr[mid])
+	{
+		return helper(arr, low, mid - 1,x);
+	}
+	return helper(arr, mid+1, high, x);
+}
+int lowerBound(vector<int> arr, int n, int x) {
+	return helper(arr, 0, n-1, x);
+}
