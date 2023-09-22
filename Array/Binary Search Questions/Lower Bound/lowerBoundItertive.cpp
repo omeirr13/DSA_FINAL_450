ab1ee69aea0716a1b053876e4acd4900664ce283
omeirr13 <74139343+omeirr13@users.noreply.github.com>
2023-09-22 14:05:44 +0500

    Update lowerBoundItertive.cpp

--- Array/Binary Search Questions/Lower Bound/lowerBoundItertive.cpp
+++ Array/Binary Search Questions/Lower Bound/lowerBoundItertive.cpp
@@ -1,1 +1,23 @@
+int lowerBound(vector<int> arr, int n, int x) {
+	//smallest index mtlb left jana
+	//x < arr[mid]
+
+
+	//lowest possible index where arr[x] >= x
+
+	int low = 0;
+	int high = n - 1;
+	int ans = n;
+	while(low <=high)
+	{
+		int mid = low + (high - low) / 2;
+		if(x <= arr[mid])
+		{
+			ans = mid;
+			high = mid - 1;
+		}
+		else low = mid + 1;
+	}
+	return ans;
+}
 
