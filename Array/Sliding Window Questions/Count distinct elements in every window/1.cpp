924a32e104e7df5a5e76aaeb9158230a24ab42aa
omeirr13 <74139343+omeirr13@users.noreply.github.com>
2023-12-15 07:49:46 +0500

    Update 1.cpp

--- Array/Sliding Window Questions/Count distinct elements in every window/1.cpp
+++ Array/Sliding Window Questions/Count distinct elements in every window/1.cpp
@@ -2,25 +2,24 @@ class Solution{
   public:
     vector <int> countDistinct (int A[], int n, int k)
     {
-        int count = 0;
-        int i = 0;
-        unordered_map<int, int> map;
-        vector<int> ans;
-        
-        for(int j = 0;j<n;j++)
-        {
-            if(map[A[j]] == 0) count++;
-            map[A[j]]++;
+       unordered_map<int, int> map;
+       vector<int> ans;
+       int i =0;
+       for(int j = 0;j<n;j++)
+       {
+           map[A[j]]++;
+           if(j - i + 1 == k)
+           {
+               ans.push_back(map.size());
+               map[A[i]]--;
             
-            if(j - i + 1 == k)
-            {
-                ans.push_back(count);
-                if(map[A[i]] == 1) count--;
-                map[A[i]]--;
-                i++;
-            }
-           
-        }
-        return ans;
+               if(map[A[i]] == 0)
+               {
+                   map.erase(A[i]);
+               }
+               i++;
+           }
+       }
+       return ans;
     }
 };
