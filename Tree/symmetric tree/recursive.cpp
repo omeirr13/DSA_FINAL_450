59b582e743c4fa944f97380c86fde4200f5e63a7
omeirr13 <74139343+omeirr13@users.noreply.github.com>
2023-12-25 08:03:50 +0500

    Update recursive.cpp

--- Tree/symmetric tree/recursive.cpp
+++ Tree/symmetric tree/recursive.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    bool helper(TreeNode* root1, TreeNode* root2)
+    {
+        if(!root1 && !root2) return true;
+        if(!root1 || !root2) return false;
+        if(root1->val != root2->val) return false;
+        return helper(root1->left, root2->right) && helper(root1->right, root2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return helper(root, root);
+    }
+};
